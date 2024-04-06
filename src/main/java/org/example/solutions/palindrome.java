c710e46ca1737461f9333b08f35ee8c8d38bc078
elmar03 <elmsoltanov@gmail.com>
2024-04-06 22:46:19 +0400

    Update

---
+++ src/main/java/org/example/solutions/palindrome.java
@@ -0,0 +1,20 @@
+package org.example.solutions;
+
+public class palindrome {
+    public boolean isPalindrome(int x) {
+        String number = Integer.toString(x);
+
+        int left = 0;
+        int right = number.length()-1;
+
+        while(left<right){
+            if(number.charAt(left)!=number.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+
+    }
+}
