a329d06aedd3e8a7deb732f050cbbdfc358674ab
elmar03 <elmsoltanov@gmail.com>
2024-04-06 22:46:19 +0400

    Initial commit

---
+++ src/main/java/org/example/solutions/twoSum.java
@@ -0,0 +1,24 @@
+package org.example.solutions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class twoSum {
+    public int[] twoSum(int[] nums, int target) {
+
+
+
+        Map<Integer,Integer> numsMap = new HashMap<>();
+
+        for(int i =0; i<nums.length;i++){
+            int complement = target-nums[i];
+
+            if(numsMap.containsKey(complement)){
+                return new int []{ numsMap.get(complement),i};
+            }
+            numsMap.put(nums[i],i);
+        }
+
+        throw new IllegalArgumentException("no match found");
+    }
+}
