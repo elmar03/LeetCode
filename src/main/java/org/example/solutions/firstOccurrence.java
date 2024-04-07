7aba1ebb1ee8acf3ebbd79d4e9e19258f69e75da
elmar03 <elmsoltanov@gmail.com>
2024-04-07 09:00:35 +0400

    Update

---
+++ src/main/java/org/example/solutions/firstOccurrence.java
@@ -0,0 +1,15 @@
+package org.example.solutions;
+
+public class firstOccurrence {
+    public int strStr(String haystack, String needle) {
+
+        int n= needle.length();
+        int k = haystack.length();
+        for(int i = 0; i<k && k-i>=n; i++){
+            if(haystack.substring(i,i+n).equals(needle)){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
